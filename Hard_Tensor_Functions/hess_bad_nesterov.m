441818895a150b3f126633b0ced768307e701ab5
cauribe2 <47286314+cauribe2@users.noreply.github.com>
2019-02-02 20:16:28 -0500

    Delete hess_bad_nesterov.m

--- Hard_Tensor_Functions/hess_bad_nesterov.m
+++
@@ -1,7 +0,0 @@
-%%
-% This function computes the Hessian of the Hard Tensor functions at a
-% particular point
-
-function hess = hess_bad_nesterov(A_k,x,p)
-
-hess = 3*A_k'*diag((A_k*x).^(p-1))*A_k;
