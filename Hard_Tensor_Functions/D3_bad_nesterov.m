770df49bd3aa28f4760ab7d318487b4b1b192237
cauribe2 <47286314+cauribe2@users.noreply.github.com>
2019-02-02 20:15:54 -0500

    Delete D3_bad_nesterov.m

--- Hard_Tensor_Functions/D3_bad_nesterov.m
+++
@@ -1,7 +0,0 @@
-%%
-% This function computes the directional third order derivative of the Hard
-% Tensor functions at a particular point x, for a particular direction h
-
-function D3 = D3_bad_nesterov(A_k,x,p,h)
-
-D3 = 6*sum(A_k'*((A_k*x).*(A_k*h).^(p-1)),2);
